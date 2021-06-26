dc256705fd7e72c6ecb796994f89c7bcc26827c6
wolf3s <75867486+wolf3s@users.noreply.github.com>
2021-06-25 23:45:56 -0300

    Initial commit

---
+++ if.cpp
@@ -0,0 +1,41 @@
+//Cálculo de equações matemática algébra por andré guilheme
+
+using namespace std;
+
+#include <iostream>
+
+int x = 0;
+int Fx = 0;
+int Gx = 0;
+
+int main()
+{
+
+   if (Fx = 0 || x == 0)
+   {
+       cout << "Qual e o valor de Fx: ";
+       cin >> x;
+
+       Fx = x - 4;
+       x - 4;
+       x = 4;
+       x = 4/2;
+   
+     cout << "O valor de Fx e: " << x << "\n";
+   
+   }
+     if (Gx == 0 || x - 0)
+     {
+
+        cout << "Qual é o valor de Fx e: ";
+        cin >> x;
+
+        Fx = x - 2;
+        x - 2;
+        x = 2;
+        
+        cout << "O Valor de Gx e: " << x << "\n"; 
+
+     } 
+}
+
