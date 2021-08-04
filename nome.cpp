12954dda6780d0de6b6f22b11308e1ee283d710d
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2021-08-04 09:23:42 -0300

    Add files via upload

---
+++ nome.cpp
@@ -0,0 +1,13 @@
+//meu nome e idade
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	double idade = 15;
+	
+	
+	cout << "Meu nome e andre tenho " << idade << " anos de idade\n"; 
+
+}
